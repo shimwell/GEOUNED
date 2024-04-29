30e538c228c06a01f7b42a85645547086aa679ff
Jonathan Shimwell <mail@jshimwell.com>
2024-04-29 13:14:18 +0100

    refactor reverse

--- src/geouned/GEOReverse/reverse.py
+++ src/geouned/GEOReverse/reverse.py
@@ -9,36 +9,38 @@ from .Modules.processInp import setOptions
 from .Modules.XMLinput import XmlInput
 
 
-def reverse(optFile="configRevese.ini"):
+def cad_to_csg_from_config(filename="config_cad_to_csg_example.ini"):
+    setting = setOptions(filename)
+
+    geom_file = setting["inputFile"]
+    out_name = setting["fileOut"]
+    out_box = setting["outBox"]
+    in_format = setting["inFormat"]
+    cad_to_csg()
+
+def cad_to_csg(geom_file, ustart, level_max, cell, mat, in_format, out_box, out_name):
 
     printCodeVersion()
 
-    setting = setOptions(optFile)
-
-    geomfile = setting["fileIn"]
-    outname = setting["fileOut"]
-    outBox = setting["outBox"]
-    inFormat = setting["inFormat"]
-
     CADselection = {
-        "Ustart": setting["UStart"],
-        "levelMax": setting["levelMax"],
-        "cell": setting["cell"],
-        "mat": setting["mat"],
-        "format": setting["inFormat"],
+        "Ustart": ustart,
+        "levelMax": level_max,
+        "cell": cell,
+        "mat": mat,
+        "format": in_format,
     }
 
     UnivCell = CadCell()
-    UnivCell.shape = UnivCell.makeBox(FreeCAD.BoundBox(*outBox))
+    UnivCell.shape = UnivCell.makeBox(FreeCAD.BoundBox(*out_box))
 
     # get geometry definition from MCNP input
-    if inFormat == "mcnp":
-        geom = McnpInput(geomfile)
-    elif inFormat == "openMC_XML":
-        geom = XmlInput(geomfile)
+    if in_format == "mcnp":
+        geom = McnpInput(geom_file)
+    elif in_format == "openMC_XML":
+        geom = XmlInput(geom_file)
     else:
         msg = (
-            f"input format type {inFormat} is not supported."
+            f"input format type {in_format} is not supported."
             'Supported options are "mcnp" or "openMC_XML"'
         )
         raise ValueError(msg)
